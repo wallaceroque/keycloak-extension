caa7d8fba2805e0127cadd885042805ca0fb618f
Wallace Roque <wallacerock@gmail.com>
2018-02-26 22:43:16 -0300

    Adicionando informações operacionais do provider

--- user-storage-rest/src/main/java/br/gov/dataprev/keycloak/storage/cidadao/CidadaoStorageProviderFactory.java
+++ user-storage-rest/src/main/java/br/gov/dataprev/keycloak/storage/cidadao/CidadaoStorageProviderFactory.java
@@ -16,22 +16,33 @@
  */
 package br.gov.dataprev.keycloak.storage.cidadao;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import javax.naming.InitialContext;
 
 import org.jboss.logging.Logger;
 import org.keycloak.Config;
 import org.keycloak.component.ComponentModel;
 import org.keycloak.models.KeycloakSession;
+import org.keycloak.provider.ServerInfoAwareProviderFactory;
 import org.keycloak.storage.UserStorageProviderFactory;
+
+import br.gov.dataprev.keycloak.storage.rest.RESTIdentityStore;
 
 /**
  * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
  * @version $Revision: 1 $
  */
-public class CidadaoStorageProviderFactory implements UserStorageProviderFactory<CidadaoStorageProvider> {
+public class CidadaoStorageProviderFactory implements 
+	UserStorageProviderFactory<CidadaoStorageProvider>,
+	ServerInfoAwareProviderFactory {
+	
     private static final Logger logger = Logger.getLogger(CidadaoStorageProviderFactory.class);
     
     private CidadaoIdentityStoreRegistry restStoreRegistry;
+
+	private String apiConnectionUrl = "";
 
     @Override
     public CidadaoStorageProvider create(KeycloakSession session, ComponentModel model) {
@@ -40,11 +51,21 @@ public class CidadaoStorageProviderFactory implements UserStorageProviderFactory
             CidadaoStorageProvider provider = (CidadaoStorageProvider)ctx.lookup("java:global/user-storage-rest/" + CidadaoStorageProvider.class.getSimpleName());
             provider.setModel(model);
             provider.setSession(session);
-            provider.setIdentityStore(this.restStoreRegistry.getRestStore(session, model));
+            RESTIdentityStore <Cidadao>identityStore = this.restStoreRegistry.getRestStore(session, model);
+            provider.setIdentityStore((CidadaoIdentityStore) identityStore);
+            this.apiConnectionUrl  = identityStore.getConfig().getConnectionUrl();
             return provider;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+    
+    @Override
+    public Map<String, String> getOperationalInfo() {
+        Map<String, String> info = new LinkedHashMap<>();
+        info.put("Versão", "1.0.0-SNAPSHOT");
+        info.put("Endereço da API REST", this.apiConnectionUrl + "");
+        return info;
     }
 
     @Override
